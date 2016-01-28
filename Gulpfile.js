96e80cd9651dc2f6d459c0061d637c9c2b09ab9f
Daniel Leu <Daniel.Leu@klm.com>
2016-01-28 10:43:26 +0100

    Removed unused stuff

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,11 +3,15 @@ const $ = require('gulp-load-plugins')({
     scope: ['devDependencies'],
     lazy: true
 });
+const connect = require('connect');
+const browserSync = require('browser-sync');
+const sStatic = require('serve-static');
 
 const JS_FILES = ['src/**/*.js'];
 const CSS_FILES = ['src/**/*.scss'];
 const HTML_FILES = ['src/**/*.html'];
 const TARGET = 'dist';
+const HTTP_PORT = '3000';
 
 // Vendor scripts, the order matters.
 const VENDOR_FILES = [
@@ -49,6 +53,30 @@ gulp.task('app-css', () => {
 gulp.task('app-index', () => {
     return gulp.src('index.html')
         .pipe(gulp.dest(TARGET));
+});
+
+gulp.task('browser-sync', () => {
+
+    var proxy = {
+        target: 'localhost:' + HTTP_PORT
+    };
+    var app = connect();
+
+    app.use(sStatic(TARGET));
+    app.listen(HTTP_PORT);
+
+    return browserSync({
+        proxy : proxy,
+        notify: false, // Do not show the the notification
+        open : false, // don't automatically open browser
+        ghostMode : false // don't make it mirror all actions accross browsers (click/forms/scroll)
+    });
+});
+
+gulp.task('watch', ['browser-sync'], () => {
+    gulp.watch(JS_FILES, ['app-js', browserSync.reload]);
+    gulp.watch(CSS_FILES, ['app-css', browserSync.reload]);
+    gulp.watch(HTML_FILES, ['app-html', browserSync.reload]);
 });
 
 gulp.task('default', $.sequence('app-html', 'vendor-js', 'app-js', 'app-css', 'app-index'));
