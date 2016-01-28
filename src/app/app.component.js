e9c4fe627377e5a7b558ba6e26919ab5a3e87530
Daniel Leu <Daniel.Leu@klm.com>
2016-01-28 11:01:57 +0100

    Remove service. Not the scope of this branch

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,11 +1,9 @@
 import {Component, OnInit, Inject} from 'angular2/core';
-import {MyAppService} from './app.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html'
 })
-@Inject(MyAppService)
 export class AppComponent implements OnInit {
   title = 'Hello World!';
 
@@ -13,6 +11,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.title = this.MyAppService.getList().join(',');
+    this.title = 'Angular2 + Babel + SystemJS'
   }
 }
--- src/app/app.service.js
+++
@@ -1,8 +0,0 @@
-import {Injectable} from 'angular2/core';
-
-@Injectable()
-export class MyAppService {
-    getList() {
-        return [1,2,3,4,5];
-    }
-}
